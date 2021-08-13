f8d1715b176fecb5299414a5b600bc9e75311003
abner-ribeiro <abner.ribeiro@zup.com.br>
2021-08-13 16:22:21 -0300

    Adicionando estrutura inicial do projeto

---
+++ src/test/java/com/zup/propostas/PropostasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.propostas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
